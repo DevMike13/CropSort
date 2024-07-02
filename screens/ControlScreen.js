15d263ffadfe36e92897c028d0f9cd87f8bf00c2
ITMike <itmike@hmanage.xyz>
2024-07-02 14:50:42 +0800

    Added firebase connection

--- screens/ControlScreen.js
+++ screens/ControlScreen.js
@@ -2,11 +2,17 @@ import { View, Text, TouchableOpacity, ImageBackground, Image, Button, TextInput
 import Modal from "react-native-modal";
 import { ScrollView } from 'react-native-gesture-handler';
 import NumericInput from 'react-native-numeric-input'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+import firebase from '../firebase';
+import LottieView from 'lottie-react-native';
 
 import ColorPicker, { Panel1, Panel5, Swatches, Preview, OpacitySlider, HueSlider } from 'reanimated-color-picker';
 
 const ControlScreen = () => {
+
+  // const [deviceId, setDeviceId] = useState(null);
 
   const [showModalOne, setShowModalOne] = useState(false);
   const [showModalTwo, setShowModalTwo] = useState(false);
@@ -24,34 +30,121 @@ const ControlScreen = () => {
   const [kgTwo, setKgTwo] = useState(1);
   const [kgThree, setKgThree] = useState(1);
 
-  // Note: 👇 This can be a `worklet` function.
+  const [converyorState, setConveyorState] = useState('OFF');
+  const [isStarting, setIsStarting] = useState();
+
   const onSelectColorOne = ({ hex }) => {
-    // do something with the selected color.
     setColorOne(hex);
     console.log(hex);
   };
 
   const onSelectColorTwo = ({ hex }) => {
-    // do something with the selected color.
     setColorTwo(hex);
     console.log(hex);
   };
 
   const onSelectColorThree = ({ hex }) => {
-    // do something with the selected color.
     setColorThree(hex);
     console.log(hex);
   };
 
-  const addKgOne = () => {
-    setKgOne(prevKgOne => prevKgOne + 1);
+  const reset = () => {
+    setColorOne('#000000');
+    setColorTwo('#000000');
+    setColorThree('#000000');
+    setKgOne(1);
+    setKgTwo(1);
+    setKgThree(1);
   }
 
-  const minusKgOne = () => {
-    setKgOne(kgOne - 1);
-  }
+  // BASKET 1
+  const setRTDBColorOne = () => {
+    const clrOne = colorOne;
+    firebase.database().ref('colorOne' ).set(clrOne);
+  };
 
+  const setRTDBWeightOne = () => {
+    const weightOne = kgOne;
+    firebase.database().ref('weightOne').set(weightOne);
+  };
+
+  // BASKET 2
+  const setRTDBColorTwo = () => {
+    const clrTwo = colorTwo;
+    firebase.database().ref('colorTwo').set(clrTwo);
+  };
+
+  const setRTDBWeightTwo = () => {
+    const weightTwo = kgTwo;
+    firebase.database().ref('weightTwo').set(weightTwo);
+  };
+
+  // BASKET 3
+  const setRTDBColorThree = () => {
+    const clrThree = colorThree;
+    firebase.database().ref('colorThree').set(clrThree);
+  };
+
+  const setRTDBWeightThree = () => {
+    const weightThree = kgThree;
+    firebase.database().ref('weightThree').set(weightThree);
+  };
+
+
+  useEffect(() => {
+    const rtdbClrOneRef = firebase.database().ref('colorOne');
+    rtdbClrOneRef.on('value', (snapshot) => {
+      const newClrOne = snapshot.val();
+      setColorOne(newClrOne);
+    });
+
+    const rtdbClrTwoRef = firebase.database().ref('colorTwo');
+    rtdbClrTwoRef.on('value', (snapshot) => {
+      const newClrTwo = snapshot.val();
+      setColorTwo(newClrTwo);
+    });
+
+    const rtdbClrThreeRef = firebase.database().ref('colorThree');
+    rtdbClrThreeRef.on('value', (snapshot) => {
+      const newClrThree = snapshot.val();
+      setColorThree(newClrThree);
+    });
+
+  }, []);
+
+  useEffect(() => {
+    // Attach listener for 'value' changes
+    const conveyorStateRef = firebase.database().ref('conveyorState');
+    conveyorStateRef.on('value', (snapshot) => {
+      const newState = snapshot.val();
+      setConveyorState(newState); // Update local state
+    });
+
+    // Clean up listener on component unmount
+    return () => {
+      conveyorStateRef.off('value'); // Detach listener
+    };
+  }, []);
+
+  const toggleConveyor = () => {
+    const newState = converyorState === 'ON' ? 'OFF' : 'ON';
+    firebase.database().ref('conveyorState').set(newState);
+    setConveyorState(newState); // Update local state immediately
+  };
   
+
+  // useEffect(() => {
+  //   const getDeviceId = async () => {
+  //     let id = await AsyncStorage.getItem('device_id');
+  //     if (!id) {
+  //       id = uuid.v4();
+  //       await AsyncStorage.setItem('device_id', id);
+  //     }
+  //     setDeviceId(id);
+  //   };
+  //   getDeviceId();
+  // }, []);
+
 
   return (
     <ImageBackground
@@ -63,6 +156,7 @@ const ControlScreen = () => {
             className="w-52 h-72"
             resizeMode='contain'
         />
+        {/* <Text>Device ID: {deviceId}</Text> */}
         <View className="w-[90%] flex items-center justify-center -mt-8 ">
 
             {/* BASKET 1 */}
@@ -151,7 +245,11 @@ const ControlScreen = () => {
                     <Swatches colors={['red', 'green', 'blue', 'violet']} />
                   </View>
                 </ColorPicker>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalOne(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalOne(false);
+                    setRTDBColorOne();
+                }}>
                   <Text className="text-white">
                     Apply
                   </Text>
@@ -170,7 +268,12 @@ const ControlScreen = () => {
                     <Swatches colors={['red', 'green', 'blue', 'violet']} />
                   </View>
                 </ColorPicker>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalTwo(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalTwo(false)
+                    setRTDBColorTwo();
+                  }}
+                >
                   <Text className="text-white">
                     Apply
                   </Text>
@@ -189,7 +292,12 @@ const ControlScreen = () => {
                     <Swatches colors={['red', 'green', 'blue', 'violet']} />
                   </View>
                 </ColorPicker>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalThree(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalThree(false)
+                    setRTDBColorThree();
+                  }}
+                >
                   <Text className="text-white">
                     Apply
                   </Text>
@@ -211,7 +319,11 @@ const ControlScreen = () => {
                     minValue={1}
                   />
                 </View>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalKgOne(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalKgOne(false)
+                    setRTDBWeightOne();
+                  }}>
                   <Text className="text-white">
                     Apply
                   </Text>
@@ -230,7 +342,12 @@ const ControlScreen = () => {
                     minValue={1}
                   />
                 </View>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalKgTwo(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalKgTwo(false);
+                    setRTDBWeightTwo();
+                  }}
+                >
                   <Text className="text-white">
                     Apply
                   </Text>
@@ -249,16 +366,39 @@ const ControlScreen = () => {
                     minValue={1}
                   />
                 </View>
-                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" onPress={() => setShowModalKgThree(false)}>
+                <TouchableOpacity className="bg-green-500 py-3 px-8 my-5 rounded-full" 
+                  onPress={() => {
+                    setShowModalKgThree(false)
+                    setRTDBWeightThree();
+                  }}
+                >
                   <Text className="text-white">
                     Apply
                   </Text>
                 </TouchableOpacity>
               </View>
             </Modal>
+
+            <Modal isVisible={converyorState == "ON" ? true : false}>
+              <View className="w-96 bg-[#f8fafc] flex justify-center items-center rounded-lg">
+                <LottieView
+                  autoPlay
+                  style={{
+                    width: 200,
+                    height: 200,
+                  }}
+                  // Find more Lottie files at https://lottiefiles.com/featured
+                  source={require('../assets/starting.json')}
+                />
+                <Text className="text-center font-black text-xl py-7 text-green-400 tracking-[3px] -mt-8">Sorting...</Text>
+              </View>
+            </Modal>
         </View>
-        <View className="mt-auto mb-40 flex flex-row">
-          <TouchableOpacity className="border-2 rounded-full py-2 px-5 flex justify-center">
+        <View className="mt-auto mb-52 flex flex-row">
+          <TouchableOpacity className="border-2 rounded-full py-2 px-5 flex justify-center mr-8" onPress={toggleConveyor}>
+            <Text className="text-center font-semibold">Start Sorting</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="border-2 rounded-full py-2 px-5 flex justify-center" onPress={reset}>
             <Text className="text-center font-semibold">Reset</Text>
           </TouchableOpacity>
         </View>
